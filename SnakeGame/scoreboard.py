3f73a77c74aafab3b59886e1f32bd9d986707d50
Vecniche <99493485+Vecniche@users.noreply.github.com>
2023-06-21 19:24:15 +0100

    Add files via upload

---
+++ SnakeGame/scoreboard.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+
+Font = ("Courier", 20, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.update_scoreboard()
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.write(f'score: {self.score}', align='center', font= Font)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align='center', font=("Courier", 20, "normal"))
+
+
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
+
+
